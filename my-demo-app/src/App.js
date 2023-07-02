4f5e398c5945c24219eb225579f09f20c65237c6
mandeepgarhwal <114212567+mandeepgarhwal@users.noreply.github.com>
2023-07-03 02:35:38 +0530

    Add files via upload

---
+++ my-demo-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>My first react application</h2>
+        <p>
+          Welcome to My first react app. this is to demonstrate the assignment 29.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
